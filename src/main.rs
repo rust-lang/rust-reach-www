c67703d64663b557f45fc6cf3e876a4e9474a3ec
Ashley Williams <ashley666ashley@gmail.com>
2018-03-20 16:53:14 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
+
+extern crate rocket;
+
+extern crate rocket_contrib;
+#[macro_use] extern crate serde_derive;
+
+use std::io;
+use std::path::{Path, PathBuf};
+
+use rocket_contrib::Template;
+use rocket::response::NamedFile;
+
+#[derive(Serialize)]
+struct Context {
+  parent: String,
+}
+
+#[get("/")]
+fn index() -> Template {
+    let page = "index".to_string();
+    let context = Context {
+      parent: "layout".to_string(),
+    };
+    Template::render(page, &context)
+}
+
+#[get("/static/<file..>", rank = 1)]
+fn files(file: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("static/").join(file)).ok()
+}
+
+fn main() {
+    rocket::ignite()
+      .attach(Template::fairing())
+      .mount("/", routes![index, files]).launch();
+}
